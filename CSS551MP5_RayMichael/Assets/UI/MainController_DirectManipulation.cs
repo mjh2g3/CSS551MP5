4f1540a433d7a67a5674b5669c908f56abb1c19f
mjh2g3 <48746074+mjh2g3@users.noreply.github.com>
2021-11-27 22:51:56 -0800

    Added the Manip Axes to World
    Issues: Get a few errors on when i release the CTRL LEFT about unable to access the axes due to being deleted. Also have not tested against the average normals

--- CSS551MP5_RayMichael/Assets/UI/MainController_DirectManipulation.cs
+++ CSS551MP5_RayMichael/Assets/UI/MainController_DirectManipulation.cs
@@ -12,31 +12,31 @@ public partial class MainController : MonoBehaviour
             //Switch On the vertex manipulation
             mModel.VertManipulation(true);
         }
-        //Step2: If LeftControl Key is Down AND user selected with LMB
-        else if (Input.GetKey(KeyCode.LeftControl) && (Input.GetMouseButtonDown(0)))
-        {
-            //Debug.Log("Holding Down the Left Control AND MouseButton is clicked");
-            //if sphere selected, set mSelected
-            mousPosX = Input.mousePosition.x;
-            mousPosY = Input.mousePosition.y;
-
-            RaycastHit hitInfo = new RaycastHit();
-            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, Mathf.Infinity);
-            if (hit)
+            //Step2: If LeftControl Key is Down AND user selected with LMB
+            if ((Input.GetMouseButtonDown(0)))
             {
-                if (hitInfo.transform.gameObject.name == "Sphere")
-                {
-                    Debug.Log(hitInfo.transform.gameObject.name);
-                    SetSelectedController(hitInfo.transform.gameObject);
-                }
-            }
+                //Debug.Log("Holding Down the Left Control AND MouseButton is clicked");
+                //if sphere selected, set mSelected
+                mousPosX = Input.mousePosition.x;
+                mousPosY = Input.mousePosition.y;
 
-        }
+                RaycastHit hitInfo = new RaycastHit();
+                bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, Mathf.Infinity);
+                if (hit)
+                {
+                    if (hitInfo.transform.gameObject.name == "Sphere")
+                    {
+                        Debug.Log(hitInfo.transform.gameObject.name);
+                        SetSelectedController(hitInfo.transform.gameObject);
+                    }
+                }
+
+            }
 
         //Step3: Obtain the nextPosition for the mouse position at mouse button hold + Alt-Left
         else if (Input.GetKey(KeyCode.LeftControl) && (Input.GetMouseButton(0)))
         {
-            //Debug.Log("Holding Down the Left Control AND MouseButton is held down");
+            Debug.Log("Holding Down the Left Control AND MouseButton is held down");
             //Step4: Compute the delta position change for the mouse using the initial and nextPosition
             float dx = mousPosX - Input.mousePosition.x;
             float dy = mousPosY - Input.mousePosition.y;
