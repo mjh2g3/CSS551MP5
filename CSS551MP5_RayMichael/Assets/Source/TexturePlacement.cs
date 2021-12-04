012b4344f3b72dc508bf59ab1eebfd3903538f6f
mjh2g3 <mjh2g3@gmail.com>
2021-12-04 02:15:17 -0800

    trs enabled

--- CSS551MP5_RayMichael/Assets/Source/TexturePlacement.cs
+++ CSS551MP5_RayMichael/Assets/Source/TexturePlacement.cs
@@ -6,6 +6,7 @@ public class TexturePlacement : MonoBehaviour {
 
     public Vector2 Offset = Vector2.zero;
     public Vector2 Scale = Vector2.one;
+    public float Rotation = 0.0f;
     Vector2[] mInitUV = null; // initial values
 
     public void SaveInitUV(Vector2[] uv)
@@ -20,12 +21,19 @@ public class TexturePlacement : MonoBehaviour {
     {
         Mesh theMesh = GetComponent<MeshFilter>().mesh;
         Vector2[] uv = theMesh.uv;
+
+
+        Matrix3x3 t = Matrix3x3Helpers.CreateTranslation(Offset);
+        Matrix3x3 s = Matrix3x3Helpers.CreateScale(Scale);
+        Matrix3x3 r = Matrix3x3Helpers.CreateRotation(Rotation);
+        
         for (int i = 0; i < uv.Length; i++)
         {
-            uv[i].x = mInitUV[i].x * Scale.x;
-            uv[i].y = mInitUV[i].y * Scale.y;
-            uv[i] = Offset + uv[i];
+
+            uv[i] = t * r * s * mInitUV[i];
+            Debug.Log("This is uv[i] at i = " + i + ":" + uv[i]);
         }
+
         theMesh.uv = uv;
     }
 }
